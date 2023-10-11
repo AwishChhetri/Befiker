37d54785100ec4d125a467ad7d8c9d999bc09321
AwishChhetri <abishchhetri.2502@gmail.com>
2023-10-11 21:30:20 +0530

    initial

---
+++ js/index..js
@@ -0,0 +1,19 @@
+
+
+const bar=document.getElementById('bar');
+const nav=document.getElementById('navbar');
+const close=document.getElementById('close');
+const active=document.getElementById('mobile');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+    
+}
+
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
